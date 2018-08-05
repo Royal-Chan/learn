18579a48c21f26e8a38ca1ad5937ab87b3b36483
陈荣耀 <royal@chenrongyaos-MacBook-Pro.local>
2018-08-05 23:18:13 +0800

    init

---
+++ sb/1st/src/main/groovy/com/imryao/learn/sb/st/Application.groovy
@@ -0,0 +1,12 @@
+package com.imryao.learn.sb.st
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application {
+
+	static void main(String[] args) {
+		SpringApplication.run Application, args
+	}
+}
